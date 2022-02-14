fd0394df7ac31583de504c39275a453753074e82
BVolt <brendenj7@hotmail.com>
2022-02-13 16:53:27 -0800

    redesign

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
 import './welcome.scss'
 
 
@@ -15,28 +14,10 @@ const Welcome = () => {
           <button className='btn' onClick={()=>window.location.href="#foot"}>Contact Me</button>
         </div>
     </div>
-
-    {/* <div className="welcome-img">
-      <img src={require('./background.jpg')} alt='tree'/>
-    </div> */}
-    {/* <div className="welcome-split">
-      <div className="welcome-contents">
-        <div className="welcome-headers">
-          <h1>Brenden Johnson</h1>
-          <h2>Web Developer</h2>
-        </div>
-        <div className="welcome-links">
-          <a href="#projects">Check Out My Projects</a>
-          <a href="#about">About Me</a>
-          <Link to="/Resume">My Resume</Link>
-          <a href="#foot">Contact Me</a>
-        </div>
-      </div>
-      <div className="img-self">
+      {/* <div className="img-self">
         <img style={{width: "400px"}}src={require('./johnsonPic.jpg')} alt='Me'/>
-      </div>
-    </div> */}
-  </div>
+      </div> */}
+    </div> 
   </>
 };
 
