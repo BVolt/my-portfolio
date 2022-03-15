05374e1ccab34ec6b311e5714fee3c5c4dfcb27b
BVolt <brendenj7@hotmail.com>
2022-03-15 09:46:23 -0700

    small changes

--- src/Nav&Footer/Navigation.js
+++ src/Nav&Footer/Navigation.js
@@ -3,7 +3,7 @@ import './nav.scss'
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 //This nav component is the top bar the sets menu conditions
 function Navigation({menu, setMenu, isResume = false}){
